38388d5a734cf739c6f3a0489839ae54e6f14abe
bdsdirect-sdm <bdsdirect.sdm@smartdatainc.net>
2024-10-14 18:33:18 +0530

    Delete asback directory

--- asback/server.ts
+++
@@ -1,5 +0,0 @@
-import app from "./src/app";
-
-app.listen(4011, () => {
-  console.log("App is running on port 40");
-});
--- asback/src/config/db.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-import dotenv from "dotenv"
-dotenv.config();
-const sequelize = new Sequelize("jobseeker", "root","Password123#@!", {
-    host: "localhost",
-    dialect: 'mysql',
-});
-
-export default sequelize;
