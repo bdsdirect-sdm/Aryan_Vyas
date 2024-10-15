02a33a88b1a6a16650899f2858d3d38dbd1e3c8e
bdsdirect-sdm <bdsdirect.sdm@smartdatainc.net>
2024-10-15 12:04:21 +0530

    Delete asfrontend directory

--- asfrontend/asbackend/src/config/db.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('usejob', 'root', 'Password123#@!', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-export default sequelize;
